df3ccea27dd66feaa0e825d60077be8e6c8ed78a
Anushka Srivastava <asrockstar555@gmail.com>
2025-08-01 21:15:05 +0530

    Made essentials page fully responsive [GSSoC’25]

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -2,8 +2,48 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
-void main() => runApp(
-  const Center(child: Text('Hello, world!', key: Key('title'), textDirection: TextDirection.ltr)),
-);
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('My First App'), backgroundColor: Colors.deepPurple),
+        backgroundColor: Colors.blueGrey[900],
+        body: Builder(
+          builder: (BuildContext context) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Hello, Flutter!',
+                    style: TextStyle(
+                      fontSize: 26,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(
+                        context,
+                      ).showSnackBar(const SnackBar(content: Text('You clicked the button!')));
+                    },
+                    child: const Text('Click Me'),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
